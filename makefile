25e1988708fc80d1e68b2eddb5ad7015b7f39830
ruiqinl <liruiqins@gmail.com>
2013-11-20 17:34:21 -0500

    msg

--- makefile
+++ makefile
@@ -1,13 +1,16 @@
 CC = gcc
 CFLAGS = -DDEBUG -Wall -g
 
-OBJS = proxy.o
+OBJS = proxy.o http_parser.o http_replyer.o helper.o
 BINS = proxy
 
 .c.o:
 	$(CC) $(CFLAGS) -c $< -o $@
 
 all: $(BINS)
+
+run:
+	./proxy logfile 0.5 8889 1.0.0.1 5.0.0.1 9999 3.0.0.1
 
 proxy: $(OBJS)
 	$(CC) $(CFLAGS) $^ -o $@
--- proxy.c
+++ proxy.c
@@ -9,28 +9,52 @@
 #include <stdio.h>
 #include <assert.h>
 
+#include "helper.h"
+#include "http_replyer.h"
+#include "http_parser.h"
 
-#define BUF_SIZE 2048
 
 int main(int argc, char *argv[]){
 
     int listen_sock;
-    int listen_port;// = 8888;
+    
     struct sockaddr_in listen_addr;
     int backlog = 5;
     int sock;
-    char buf[BUF_SIZE];
+    //char buf[BUF_SIZE];
     int numbytes;
     struct sockaddr_in cli_addr;
     socklen_t cli_size;
 
-    if (argc < 2) {
-	printf("Usage: ./proxy <proxy_port>\n");
+    int listen_port;// = 8888;
+    char *fake_ip = NULL;
+    char *dns_ip = NULL;
+    int dns_port;
+    char *www_ip = NULL;
+
+    struct sockaddr_in server_addr;
+    int sock2server;
+
+    int maxfd;
+    fd_set read_fds, write_fds;
+    fd_set master_read_fds, master_write_fds;
+
+    struct buf* buf_pts[MAX_SOCK];
+    int i;
+    
+    // parse argv
+    if (argc < 8) {
+	printf("Usage: ./proxy <log> <alpha> <listen_port> <fake-ip> <dns-ip> <dns-port> <www-ip>\n");
 	return -1;
     }
 
-    listen_port = atoi(argv[1]);
-    
+    listen_port = atoi(argv[3]);
+    fake_ip = argv[4];
+    dns_ip = argv[5];
+    dns_port = atoi(argv[6]);
+    www_ip = argv[7];
+
+    // browser side of proxy
     listen_sock = socket(AF_INET, SOCK_STREAM, 0);
 
     memset(&listen_addr, 0, sizeof(listen_addr));
@@ -43,20 +67,67 @@ int main(int argc, char *argv[]){
 	exit(-1);
     }
 
-    listen(listen_sock, backlog);
+    if (listen(listen_sock, backlog) == -1) {
+	perror("Error! main, listen");
+	exit(-1);
+    }
     
     if((sock = accept(listen_sock, (struct sockaddr *)&cli_addr, &cli_size)) == -1){
 	perror("Error! main, accpet");
 	exit(-1);
     }
 
+    // server side of proxy: addr
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    inet_aton(www_ip, &server_addr.sin_addr);
     
-    if ((numbytes = recv(sock, buf, BUF_SIZE, 0)) == -1) {
-	perror("Error! main, recv\n");
+    // server side of proxy: whatever, get f4m first
+    if((sock2server = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+	perror("Error! main, socket, socket2server");
 	exit(-1);
     }
 
-    printf("proxy receives from browser:%s\n", buf);
+    if (connect(sock2server, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+	perror("Error! main, connect, socket2server");
+	exit(-1);
+    }
+    
+    getf4m(sock2server);
+    
+    // receive connection from browser
+    
+    while (1) {
+	
+	read_fds = master_read_fds;
+	write_fds = master_write_fds;
+	
+	if (select(maxfd+1, &read_fds, &write_fds, NULL, NULL) == -1) {
+	    perror("Error! main, select");
+	    close_socket(sock);
+	    return(-1);
+	}
+
+	for (i = 0; i <= maxfd; i++) {
+	    
+	    // check read_fds
+	    if (FD_ISSET(i, &read_fds)) {
+		
+		if (i == listen_sock) {
+		    printf("main: received new connection from browser\n");
+		} else 
+		    printf("main: received bytes from browser/server\n");
+	    }
+
+	    // check write_fds
+	    if (FD_ISSET(i, &write_fds)) {
+		
+		printf("main: write bytes to browser/server\n");
+	    }
+	}
+
+    }
+    
     
     close(sock);
     close(listen_sock);
