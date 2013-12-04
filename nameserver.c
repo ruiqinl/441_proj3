7d9ce4843067a9d9f4167356e5928b8076b02bf2
Ruiqin Li <ruiqinli@cmu-785011.wv.cc.cmu.edu>
2013-12-04 04:27:20 -0500

    msg

--- nameserver.c
+++ nameserver.c
@@ -125,6 +125,9 @@ int main(int argc, char *argv[]) {
 	
 	client_ind = get_client_ind(&client_addr, ip_list);
 	server_ind_list = get_server_ind(serverlist, ip_list);
+	printf("get_client_ind:%s, %d\n", client_ip, client_ind);
+	printf("server_ind_list:");
+	print_list(server_ind_list, printer_ind);
 
 	server_ind = do_dijkstra(graph, graph_size, client_ind, server_ind_list);
 	
@@ -199,8 +202,6 @@ int get_client_ind(struct sockaddr_in *client_addr, struct list_node_t *ip_list)
   client_ind = list_ind(ip_list, client_ip, comparor_str);
   assert(client_ind != -1);
 
-  printf("get_client_ind:%s, %d\n", client_ip, client_ind);
-
   return client_ind;
 }
 
@@ -228,7 +229,7 @@ struct list_node_t *get_server_ind(struct server_t *serverlist, struct list_node
 
     server_p = server_p->next; 
 
-    dbprintf("%s_%d, ", ip, *ind);
+    //dbprintf("%s_%d, ", ip, *ind);
   }
   printf("\n");
  
